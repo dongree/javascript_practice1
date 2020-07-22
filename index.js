610a4ac204a37ffe75f7a2cfab81a8170426d6b9
dongree <68095803+dongree@users.noreply.github.com>
2020-07-22 12:51:35 +0900

    version 2

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const title = document.querySelector("#title");
 
-
-const BASE_COLOR = "white";
-const OTHER_COLOR = "tomato";
+const CLICKED_CLASS ="clicked";
 
 function handleClick(){
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR){
-    title.style.color = OTHER_COLOR;
+  const hasClass = title.classList.contains(CLICKED_CLASS);
+  if(hasClass){
+    title.classList.remove(CLICKED_CLASS);
   } else{
-    title.style.color = BASE_COLOR;
+    title.classList.add(CLICKED_CLASS);
   }
 }
+
 function init(){
-  title.style.color = BASE_COLOR;
+
   title.addEventListener("click", handleClick);
 }
 init();
